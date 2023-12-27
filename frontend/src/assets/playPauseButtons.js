7b64f75503156482cae69c83afbc65465d7e21de
R M <rmutlu01@hotmail.com>
2023-12-27 19:51:28 +0200

    Post layout done

---
+++ frontend/src/assets/playPauseButtons.js
@@ -0,0 +1,11 @@
+export const playButtonSVG = `
+  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-player-play-filled" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    <path d="M6 4v16a1 1 0 0 0 1.524 .852l13 -8a1 1 0 0 0 0 -1.704l-13 -8a1 1 0 0 0 -1.524 .852z" stroke-width="0" fill="currentColor" />
+  </svg>`;
+
+export const pauseButtomSVG = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-player-pause-filled" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+<path d="M9 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" stroke-width="0" fill="currentColor" />
+<path d="M17 4h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h2a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2z" stroke-width="0" fill="currentColor" />
+</svg>`;
--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Login.css';
-import { LoginFormValues } from "../../types";
+import { LoginFormValues } from '../../types';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 //import { Link } from "react-router-dom";
 const Login = () => {
-    const initialValues: LoginFormValues = {username: '', password: ''};
+    const initialValues: LoginFormValues = { username: '', password: '' };
     const handleRegister = (values: LoginFormValues) => {
         console.log('Values: ', values );
         alert(JSON.stringify(values, null, 2));
@@ -12,20 +12,20 @@ const Login = () => {
     return (
         <div className="form-main">
             <Formik initialValues={initialValues} className="loginForm"
-            onSubmit={handleRegister}>
-                    
+                onSubmit={handleRegister}>
+
                 <Form className="form">
 
-                <label htmlFor="username">Username</label>
-                <Field type="text" className="formInput"  name="username" />
-                <ErrorMessage name="username" component='div' className="error"/>
-                
+                    <label htmlFor="username">Username</label>
+                    <Field type="text" className="formInput"  name="username" />
+                    <ErrorMessage name="username" component='div' className="error"/>
 
-                <label htmlFor="password">Password</label>
-                <Field type="password" className="formInput"   name="password" />
-                <ErrorMessage name="password" component='div' className="error"/>
-                
-                <button type="submit" className="formButton">Sign In</button>
+
+                    <label htmlFor="password">Password</label>
+                    <Field type="password" className="formInput"   name="password" />
+                    <ErrorMessage name="password" component='div' className="error"/>
+
+                    <button type="submit" className="formButton">Sign In</button>
                 </Form>
             </Formik>
             <p>Not a user?</p>
