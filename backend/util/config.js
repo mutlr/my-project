f8c7a174583cef1cfa4683cf3aa218d27aed138e
Rojhat M <rmutlu01@hotmail.com>
2023-12-28 03:14:04 +0200

    URL

--- backend/util/config.js
+++ backend/util/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-	DATABASE_URL: process.env.DATABASE_URL,
+	DATABASE_URL: process.env.DATABASE_URL || 'postgres://postgres:mysecretpassword@localhost:5432/postgres',
 	PORT: process.env.PORT || 3001,
 	SECRET: process.env.SECRET,
 };
