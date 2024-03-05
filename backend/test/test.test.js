be3a83ecb7ba602030f48029578ca724b5c3389f
mutlr <rmutlu01@hotmail.com>
2024-03-05 13:48:12 +0200

    Register ja login testei, ei valmis

--- backend/test/test.test.js
+++ backend/test/test.test.js
@@ -1,5 +1,5 @@
 const server = require('../app.js');
-const { connectToDatabase, sequelize, runMigrations } = require('../util/db');
+const { connectToDatabase } = require('../util/db');
 const supertest = require('supertest');
 const { emptyDatabase, initDatabase } = require('./hooks.js');
 let api = supertest(server);
@@ -16,14 +16,44 @@ describe('User Endpoints', () => {
 		expect(res.body).toHaveProperty('users');
 		expect(res.body.users.length).toBe(2);
 	});
-	it('Register', async () => {
-		const user = { username: 'User3', email: 'user2@hotmail.com', password: 'user123' }
-		const res = await api.post('/register').send(user);
-		expect(res.status).toEqual(201)
-		expect(res.status.body).toHaveProperty('token')
-		console.log('Res from users test: ', res.body);
-		 
+	describe('Registering tests', () => {
+		it('No duplicate emails or usernames', async () => {
+			const user = { username: 'User3', email: 'user2@hotmail.com', password: 'user123' }
+			const emailDuplicate = await api.post('/register').send(user).expect(400);
+			expect(emailDuplicate.error).toBeDefined()
+	
+			user.username = 'User1';
+			user.email = 'Notaduplicate@hotmail.com';
+			const usernameDuplicate = await api.post('/register').send(user).expect(400);
+			expect(usernameDuplicate.error).toBeDefined()
+		});
+	
+		it('Register fails with missing data', async () => {
+			const user = { username: 'User4', email: 'user4@hotmail.com' };
+			await api.post('/register').send(user).expect(500);
+		})
+		it('Register', async () => {
+			const user = { username: 'User3', email: 'user3@hotmail.com', password: 'user123' }
+			const res = await api.post('/register').send(user).expect(201);
+			expect(res.body).toHaveProperty('token')
+		});
 	});
+	describe('Login tests', () => {
+		it('Login with working credintials', async () => {
+			const user = { username: 'User3', password: 'user123' };
+			const result = await api.post('/login').send(user).expect(200);
+
+			expect(result.body).toBeDefined()
+			expect(result.body).toHaveProperty('token');
+			expect(result.body).toHaveProperty('username', 'User3');
+		});
+		it('Login with invalid username', async () => {
+			const user = { username: 'Use', password: 'user123' };
+			const i = await api.post('/login').send(user)
+			expect(i.body).toThrow(new Error('Invalid username or password'));
+
+		})
+	})
 	afterAll(async () => {
 		await emptyDatabase();
 	});
